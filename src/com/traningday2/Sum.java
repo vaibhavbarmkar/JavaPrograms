6c656ec0e3f8e84094eff8e450314c93820c617a
vaibhavbarmkar <vaibhav.barmkar@gmail.com>
2015-11-21 18:06:58 +0530

    Added sum class

---
+++ src/com/traningday2/Sum.java
@@ -0,0 +1,15 @@
+package com.traningday2;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a=10;
+     int b=10;
+    int  sum = a+b;
+    
+    System.out.println(sum);
+      
+	}
+
+}
